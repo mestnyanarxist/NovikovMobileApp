b655e6a83d81660445de965a25ad1539b243885b
Mixail <rodgdestwo@yandex.ru>
2023-03-20 01:13:24 +0300

    Lab 2 complete

--- screens/WelcomeScreen/WelcomeScreen.jsx
+++ screens/WelcomeScreen/WelcomeScreen.jsx
@@ -1,7 +1,23 @@
+import {gql, useQuery } from '@apollo/client'
 import {View, Text} from 'react-native'
 
+const GET_EVENT = gql`
+    query getEvent {
+        event(id: 10){
+            name,
+            source,
+            description
+        }
+    }
+`;
+
 export const WelcomeScreen = () => {
+
+    const data = useQuery(GET_EVENT);
+
+    console.log(data);
+
     return <View>
-        <Text>Welcome screen</Text>
+        <Text>{data}</Text>
     </View>
 }
